ec64491071163337a18b126e5f364af28f733b3f
aminhbl <amin.habibllh@gmail.com>
2021-02-15 16:07:07 +0330

    feat: create endpoints and structs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 **REST-API**
 ***
-~~~~
-..* Developed with GoLang.
-..* Using [gorilla/mux](https://github.com/gorilla/mux) as the third party router.
+
+* Developed with GoLang
+  * Using [gorilla/mux](https://github.com/gorilla/mux) as the third party router.
--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Music struct {
+	ID   string `json:"id"`
+	Title   string `json:"title"`
+	Singer   *Singer `json:"singer"`
+}
+
+type Singer struct {
+	Firstname   string `json:"firstname"`
+	Lastname   string `json:"lastname"`
+	Record   string `json:"record"`
+}
+
+func main()  {
+	router := mux.NewRouter()
+
+	//Endpoints
+	router.HandleFunc("/api/musics", getMusics).Methods("GET")
+	router.HandleFunc("/api/musics/{id}", getMusic).Methods("GET")
+	router.HandleFunc("/api/musics", createMusic).Methods("POST")
+	router.HandleFunc("/api/musics/{id}", updateMusic).Methods("PUT")
+	router.HandleFunc("/api/musics/{id}", deleteMusic).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
